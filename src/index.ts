71ccdcd1ce56c758f63bacce8787857bb27709a8
SHANTO612 <fahadshanto653@gmail.com>
2024-12-19 15:51:48 +0600

    Created basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import {add} from '@/test/math'
-const a=10
-const b=20
-console.log("this is my final result:",add(a,b))
+import express from 'express'
+const app=express()
+const port=process.env.PORT||8989
+app.get('/',(request,response)=>{
+response.send("<h1> hello this is our app</h1>")
+})
+app.get('/login',(request,response)=>{
+    response.send("<h1> hello this is our Login page</h1>")
+    })
+app.listen(port,()=>{
+    console.log(`application is running on port http://localhost:${port}`)
+})
